f0eab27ec796f7ffe1d8a3261420d09893941126
Nick Aversano <nick23hi@gmail.com>
2016-10-05 02:47:57 -0400

    changed to craft

--- backend/db/migrate/20161005051307_create_gotos.rb
+++ backend/db/migrate/20161005051307_create_gotos.rb
@@ -2,10 +2,10 @@ class CreateGotos < ActiveRecord::Migration[5.0]
   def change
     create_table :gotos do |t|
       t.belongs_to :user, index: true
-      t.string :for
+      t.string :craft
+      t.string :image
       t.string :name
       t.string :nickname
-      t.string :image
       t.timestamps
     end
   end
