5ad4a5d729e5f20f475a8a004d3b702da681ac2e
Nick Aversano <nick23hi@gmail.com>
2016-10-05 03:33:43 -0400

    goto actions

--- backend/app/controllers/gotos_controller.rb
+++ backend/app/controllers/gotos_controller.rb
@@ -1,5 +1,6 @@
 class GotosController < ApplicationController
   before_action :find_user_by_nickname
+  before_action :find_goto, except: [:index]
   before_action :current_user_can_edit!, only: [:create, :update, :destroy]
 
   def index
@@ -7,16 +8,29 @@ class GotosController < ApplicationController
   end
 
   def create
+    @user.gotos.create(goto_params)
   end
 
   def update
+    @goto.update(goto_params)
+    @user.save
   end
 
   def destroy
+    @goto.delete
+    @user.save
   end
 
   private
   def find_user_by_nickname
     @user = User.find_by(nickname: params[:user_id])
+  end
+
+  def find_goto
+    @goto = Goto.find(params[:id])
+  end
+
+  def goto_params
+    params.require(:nickname, :craft).permit(:image, :name)
   end
 end
