22509d849c799dccc38a4620ecd8f090bdacbd37
Jeff Hilnbrand <jahilnbrand@gmail.com>
2016-10-05 22:36:27 -0700

    focus

--- frontend/src/js/Row.js
+++ frontend/src/js/Row.js
@@ -3,10 +3,33 @@ import EditableTextField from './EditableTextField';
 import Account from './Account';
 
 class Row extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focus: false
+    };
+    this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+  }
+
+  onFocus() {
+    this.setState({focus: true});
+  }
+
+  onBlur() {
+    this.setState({focus: false});
+  }
+
   render() {
+    let className = 'row ';
+    if (this.state.focus) className += 'editing';
     return (
       <a target="_blank" className="row-link">
-        <li className="row">
+        <li
+          className={className}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+        >
           <EditableTextField className="craft" text={this.props.skill} />
           <Account {...this.props} />
         </li>
