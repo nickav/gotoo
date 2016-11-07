0e6de6b5a7483456802f3318c70dd7af27c4bb7a
Jeff Hilnbrand <jahilnbrand@gmail.com>
2016-11-06 21:07:18 -0800

    update font, welcome page

--- frontend/src/js/components/WelcomePage.js
+++ frontend/src/js/components/WelcomePage.js
@@ -9,7 +9,6 @@ const WelcomePage = (props) => (
         <img src="/img/logo.svg" alt="Goto" />
       </h1>
       <h2 className="tagline">Your go-to people for all things.</h2>
-      <p className="subtitle">Reserve your page now.</p>
       <a href="/users/auth/twitter" className="btn large">
         <i className="icon"><img src="/img/twitter.svg" /></i>
         <span>Connect with Twitter</span>
